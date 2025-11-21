2edbc3cc9fc2830788a2a6731e1cbaeca75eceb2
NjilaBoris <njilaboris2@gmail.com>
2025-11-21 23:23:59 +0100

    Implement Authentification Socials

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,8 +1,20 @@
-import { SignInView } from "@/modules/auth/ui/views/sign-in-view";
-import React from "react";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-const SignIn = () => {
+import { auth } from "@/lib/auth";
+
+import { SignInView } from "@/modules/auth/ui/views/sign-in-view";
+
+const Page = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!!session) {
+    redirect("/");
+  }
+
   return <SignInView />;
 };
 
-export default SignIn;
+export default Page;
