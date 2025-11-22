75bfa5dfc4cf278c8336017d96df1699a6582bdc
NjilaBoris <njilaboris2@gmail.com>
2025-11-22 14:08:15 +0100

    Implement Dashboard Navbar

---
+++ src/modules/dashboard/ui/components/dashboard-command.tsx
@@ -0,0 +1,76 @@
+"use client";
+import { useRouter } from "next/navigation";
+
+import { Dispatch, SetStateAction, useState } from "react";
+
+import {
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandGroup,
+  CommandEmpty,
+  CommandDialog,
+} from "@/components/ui/command";
+
+interface Props {
+  open: boolean;
+  setOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+export const DashboardCommand = ({ open, setOpen }: Props) => {
+  const router = useRouter();
+  const [search, setSearch] = useState("");
+
+  return (
+    <CommandDialog open={open} onOpenChange={setOpen}>
+      <CommandInput
+        placeholder="Find a meeting or agent..."
+        value={search}
+        onValueChange={(value) => setSearch(value)}
+      />
+      <CommandList>
+        <CommandGroup heading="Meetings">
+          <CommandEmpty>
+            <span className="text-muted-foreground text-sm">
+              No meetings found
+            </span>
+          </CommandEmpty>
+          {/* {meetings.data?.items.map((meeting) => (
+            <CommandItem
+              onSelect={() => {
+                router.push(`/meetings/${meeting.id}`);
+                setOpen(false);
+              }}
+              key={meeting.id}
+            >
+              {meeting.name}
+            </CommandItem>
+          ))} */}
+        </CommandGroup>
+        <CommandGroup heading="Agents">
+          <CommandEmpty>
+            <span className="text-muted-foreground text-sm">
+              No agents found
+            </span>
+          </CommandEmpty>
+          {/* {agents.data?.items.map((agent) => (
+            <CommandItem
+              onSelect={() => {
+                router.push(`/agents/${agent.id}`);
+                setOpen(false);
+              }}
+              key={agent.id}
+            >
+              <GeneratedAvatar
+                seed={agent.name}
+                variant="botttsNeutral"
+                className="size-5"
+              />
+              {agent.name}
+            </CommandItem>
+          ))} */}
+        </CommandGroup>
+      </CommandList>
+    </CommandDialog>
+  );
+};
