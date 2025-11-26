76f865eaeb0f61b3bf142cc3e088c115a6ab223e
NjilaBoris <njilaboris2@gmail.com>
2025-11-26 21:27:21 +0100

    Implement Agent Filters

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -1,40 +1,57 @@
 "use client";
 
-import { ErrorState } from "@/components/errorState";
-import { LoadingState } from "@/components/loadingState";
-import { useTRPC } from "@/trpc/client";
+import { useRouter } from "next/navigation";
 import { useSuspenseQuery } from "@tanstack/react-query";
-import router from "next/router";
+
+import { useTRPC } from "@/trpc/client";
+
 import { columns } from "../components/columns";
-import { DataTable } from "../components/data-table";
+
+import { useAgentsFilters } from "../../hooks/use-agents-filters";
 import { EmptyState } from "@/components/empty-state";
+import { DataTable } from "../components/data-table";
+import { LoadingState } from "@/components/loadingState";
+import { ErrorState } from "@/components/errorState";
+import { DataPagination } from "../components/data-pagination";
 
-const AgentsView = () => {
+export const AgentsView = () => {
+  const router = useRouter();
+  const [filters, setFilters] = useAgentsFilters();
+
   const trpc = useTRPC();
-  const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
-  <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
-    <DataTable
-      data={data}
-      columns={columns}
-      onRowClick={(row) => router.push(`/agents/${row.id}`)}
-    />
+  const { data } = useSuspenseQuery(
+    trpc.agents.getMany.queryOptions({
+      ...filters,
+    })
+  );
 
-    {data.length === 0 && (
-      <EmptyState
-        title="Create your first agent"
-        description="Create an agent to join your meetings. Each agent will follow your instructions and can interact with participants during the call."
+  return (
+    <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
+      <DataTable
+        data={data.items}
+        columns={columns}
+        onRowClick={(row) => router.push(`/agents/${row.id}`)}
       />
-    )}
-  </div>;
+      <DataPagination
+        page={filters.page}
+        totalPages={data.totalPages}
+        onPageChange={(page) => setFilters({ page })}
+      />
+      {data.items.length === 0 && (
+        <EmptyState
+          title="Create your first agent"
+          description="Create an agent to join your meetings. Each agent will follow your instructions and can interact with participants during the call."
+        />
+      )}
+    </div>
+  );
 };
-
-export default AgentsView;
 
 export const AgentsViewLoading = () => {
   return (
     <LoadingState
       title="Loading Agents"
-      description="This may take a fews seconds"
+      description="This may take a fews econds"
     />
   );
 };
